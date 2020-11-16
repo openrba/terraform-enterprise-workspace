56cd23de7ed09121ded711ea9d186acd449f965d
Joshua Hiscocks <joshua.hiscocks@lexisnexisrisk.com>
2020-11-16 10:35:05 +0000

    Teams added to workspace and token set in vars

--- main.tf
+++ main.tf
@@ -10,6 +10,44 @@ resource "tfe_workspace" "workspace" {
   
 }
 
+# Workspace Team & Access
+locals {
+  access_levels = ["read", "write", "plan"]
+}
+
+resource "tfe_team" "workspace" {
+  for_each = toset(local.access_levels)
+
+  name         = "${var.name}-${each.key}"
+  organization = "Infrastructure"
+}
+
+resource "tfe_team_access" "workspace" {
+  for_each = toset(local.access_levels)
+
+  access       = each.key
+  team_id      = tfe_team.workspace[each.key].id
+  workspace_id = tfe_workspace.workspace.id
+}
+
+# Team Token
+resource "tfe_team_token" "workspace" {
+  for_each = toset(local.access_levels)
+
+  team_id = tfe_team.workspace[each.key].id
+}
+
+resource "tfe_variable" "team_token" {
+  for_each = toset(local.access_levels)
+  
+  key          = "tfe_team_token_${each.key}"
+  value        = tfe_team_token.workspace[each.key].token
+  category     = "terraform"
+  workspace_id = tfe_workspace.workspace.id
+  sensitive    = true
+  description  = "Terraform Enteprise Workspace - ${each.key} Team Token"
+}
+
 # Terraform Variables
 resource "tfe_variable" "default_connection_info" {
   key          = "default_connection_info"
