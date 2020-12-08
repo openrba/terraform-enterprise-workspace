54090952dba1357c809e923c10a3860411432c90
Joshua Hiscocks <joshua.hiscocks@lexisnexisrisk.com>
2020-12-08 19:40:56 +0000

    AD Group creation changed to Group Data Lookup

--- main.tf
+++ main.tf
@@ -21,19 +21,17 @@ data "azuread_service_principal" "guestsync" {
   display_name = "ris-azr-app-infrastructure-guestsync"
 }
 
-resource "azuread_group" "team" {
+data "azuread_group" "teams" {
   for_each = toset(local.access_levels)
 
-  name                    = "ris-azr-group-tfe-${var.name}-${each.key}"
-  owners                  = [data.azuread_service_principal.guestsync.id]
-  prevent_duplicate_names = true
+  name = "ris-azr-group-tfe-${var.name}-${each.key}"
 }
 
 # Workspace Team & Access
 resource "tfe_team" "workspace" {
   for_each = toset(local.access_levels)
 
-  name         = azuread_group.team[each.key].id
+  name         = data.azuread_group.teams[each.key].id
   organization = local.organization
   visibility   = "organization"
 }
